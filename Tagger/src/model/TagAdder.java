5c977daa7c4b7db824f541a7ecce349c374c0e66
jdvp <jdrocks94@gmail.com>
2014-10-05 01:34:09 -0500

    Changed ID3 APIs
    Working with a better, more powerful API

--- Tagger/src/model/TagAdder.java
+++ Tagger/src/model/TagAdder.java
@@ -1,19 +1,21 @@
 package model;
 
-import java.io.File;
+import java.io.RandomAccessFile;
 
-import entagged.audioformats.AudioFile;
-import entagged.audioformats.AudioFileIO;
-import entagged.audioformats.exceptions.CannotReadException;
-import entagged.audioformats.exceptions.CannotWriteException;
+import org.farng.mp3.MP3File;
+import org.farng.mp3.id3.ID3v2_3;
 
 public class TagAdder {
 
-	AudioFile audioFile = null;
+	MP3File myFile = null;
+	ID3v2_3 myTag = null;
 	String song = "";
 	
 	public TagAdder(String song)
 	{
+		this.song = song;
+		
+		/*
 		this.song = song;
 		//Retrieve song
 		try 
@@ -23,11 +25,27 @@ public class TagAdder {
 		catch(CannotReadException e)
 		{
 			System.err.println("Error reading song " + song);
-		}
+		}*/
 	}
-	
+
 	public void commitComment(String comment)
 	{
+		
+		myTag = new ID3v2_3();
+		myTag.setSongComment(comment);
+		try 
+		{
+			RandomAccessFile me = new RandomAccessFile(song, "rw");
+			myTag.write(me);
+			me.close();
+		} 
+		catch (Exception e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		/*
 		//Set comment field
 		audioFile.getTag().setComment(comment); //Sets the genre to Prog. Rock, note the file on disk is still unmodified.
 		
@@ -39,6 +57,6 @@ public class TagAdder {
 		catch(CannotWriteException e)
 		{
 			System.err.println("Error writing to song " + song);
-		}
+		}*/
 	}
 }
